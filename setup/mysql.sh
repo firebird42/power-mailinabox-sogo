ba51b8770e4c2abaeaf4423fbeb7e9485e806796
Paul Nettleton <paulnett7@hotmail.com>
2022-01-12 04:00:00 -0600

    Don't move MySQL dir in mysql.sh

--- setup/mysql.sh
+++ setup/mysql.sh
@@ -19,30 +19,30 @@ MYSQL_DATADIR=$STORAGE_ROOT/mail/mysql/
 if [ ! -d $MYSQL_DATADIR ]; then
     # Move the datadir to $STORAGE_ROOT/mail/mysql/ for backup reasons
 
-    # Stop the MySQL daemon as we're editing the config file
-    service mysql stop >> /dev/null
-
-    # Change the datadir location for MySQL
-    management/editconf.py /etc/mysql/mysql.conf.d/mysqld.cnf datadir=$MYSQL_DATADIR
-
+    # # Stop the MySQL daemon as we're editing the config file
+    # service mysql stop >> /dev/null
+    #
+    # # Change the datadir location for MySQL
+    # management/editconf.py /etc/mysql/mysql.conf.d/mysqld.cnf datadir=$MYSQL_DATADIR
+    #
     # Create the new database location in our $STORAGE_ROOT
     mkdir -p $MYSQL_DATADIR
-
-    # Move the data
-    mv /var/lib/mysql/* $MYSQL_DATADIR
-
-    # Make sure MySQL daemon has proper rights
-    chown -R mysql:mysql $MYSQL_DATADIR
-
-    # Make emtpy dir to fool the mysql daemon into thinking these exist
-    mkdir -p /var/lib/mysql/mysql
-
-    # Help apparmor detect the new MySQL home and then restart apparmor
-    echo "alias /var/lib/mysql/ -> $MYSQL_DATADIR," >> /etc/apparmor.d/tunables/alias
-    restart_service apparmor
-
-    # Restart the MySQL daemon
-    restart_service mysql
+    #
+    # # Move the data
+    # mv /var/lib/mysql/* $MYSQL_DATADIR
+    #
+    # # Make sure MySQL daemon has proper rights
+    # chown -R mysql:mysql $MYSQL_DATADIR
+    #
+    # # Make emtpy dir to fool the mysql daemon into thinking these exist
+    # mkdir -p /var/lib/mysql/mysql
+    #
+    # # Help apparmor detect the new MySQL home and then restart apparmor
+    # echo "alias /var/lib/mysql/ -> $MYSQL_DATADIR," >> /etc/apparmor.d/tunables/alias
+    # restart_service apparmor
+    #
+    # # Restart the MySQL daemon
+    # restart_service mysql
 
     # Create the MIAB and SOGo database
     mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE ${MIAB_SQL_DB}; CREATE DATABASE ${SOGO_SQL_DB};" >> /dev/null
